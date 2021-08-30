4f7185aff7bf5f1029d1ef988b5a08e55dc3df11
Josh White <jojawhi@gmail.com>
2021-08-30 00:19:41 -0700

    added auto generate buttons for 64 and 100 grids, refactored js so number of rows and columns is set by button click rather than in the createGrid function

--- etch.js
+++ etch.js
@@ -23,6 +23,26 @@ function createCells(numberOfCells) {
 createCells(16);
 */
 
+//Random colour mode
+
+const funkyBtn = document.querySelector("#funkyBtn");
+
+funkyBtn.addEventListener("click", () => {
+    alert("funkyBtn pressed");
+});
+
+function setRandomBackground() {
+    const randomColour = Math.floor(Math.random()*16777215).toString(16);
+    gridCell.addEventListener("mouseenter", () => {
+        gridCell.style.backgroundColor = "#" + randomColour + " !important";
+    });
+}
+
+// Grid Functions
+
+let numberOfRows = 0;
+let numberOfColumns = 0;
+
 function setRows(numberOfRows) {
     gridContainer.style.cssText = `grid-template-rows: repeat(${numberOfRows}, minmax(10px, 50%))`;
 }
@@ -31,30 +51,39 @@ function setColumns(numberOfColumns) {
     gridContainer.style.cssText = `grid-template-columns: repeat(${numberOfColumns}, minmax(10px, 50%))`;
 }
 
+
 function createCell() {
     let gridCell = document.createElement("div");
     gridCell.classList.add("cell");
     gridContainer.appendChild(gridCell);
+    
+    //Random colour
+    /*
+    const randomColour = Math.floor(Math.random()*16777215).toString(16);
+    gridCell.addEventListener("mouseenter", () => {
+        gridCell.style.backgroundColor = "#" + randomColour;
+    });
+    */
+
+    
     gridCell.addEventListener("mouseenter", () => {
         gridCell.classList.add("hoverCell");
     });
+    
+    /*
     gridCell.addEventListener("mouseleave", () => {
         gridCell.classList.remove("hoverCell");
     });
+    */
 }
-
 
 
 function createGrid() {
 
-    let numberOfCells = document.getElementById("cellInput").value;
-    setRows(numberOfCells);
-    setColumns(numberOfCells);
-
     let i = 0;
 
-    if ( numberOfCells <= 100 && numberOfCells >= 2) {
-        while (i < (numberOfCells * numberOfCells)) {
+    if (numberOfRows <= 100 && numberOfRows >= 2 && numberOfColumns <= 100 && numberOfColumns >= 2) {
+        while (i < (numberOfRows * numberOfColumns)) {
             createCell();
             i++;
         }
@@ -63,15 +92,47 @@ function createGrid() {
     }
 }
 
-const generateBtn = document.querySelector("#generateBtn");
-generateBtn.addEventListener("click", () => {
+const customGridBtn = document.querySelector("#customGridBtn");
+
+customGridBtn.addEventListener("click", () => {
+
+    numberOfRows = document.getElementById("rowInput").value;
+    numberOfColumns = document.getElementById("colInput").value;
+
+    setRows(numberOfRows);
+    setColumns(numberOfColumns);
+
+    gridContainer.textContent = "";
+    createGrid();
+
+});
+
+// Auto-generate buttons
+
+const grid64Btn = document.querySelector("#grid64Btn");
+grid64Btn.addEventListener("click", () => {
+
+    numberOfRows = 64;
+    numberOfColumns = 64;
+
+    setRows(numberOfRows);
+    setColumns(numberOfColumns);
+    gridContainer.textContent = "";
     createGrid();
 });
 
 
 
+const grid100Btn = document.querySelector("#grid100Btn");
+grid100Btn.addEventListener("click", () => {
 
+    numberOfRows = 100;
+    numberOfColumns = 100;
 
-
+    setRows(numberOfRows);
+    setColumns(numberOfColumns);
+    gridContainer.textContent = "";
+    createGrid();
+});
 
 
