f2faff82180dccc686a234ed86cc1c1c6196a3ca
Josh White <jojawhi@gmail.com>
2021-08-29 17:05:20 -0700

    refactored js to allow user to set their own grid dimensions, made the createCell function and changed createCells to createGrid

--- etch.js
+++ etch.js
@@ -1,5 +1,6 @@
 const gridContainer = document.querySelector("#gridContainer");
 
+/*
 function createCells(numberOfCells) {
     
     let i = 0;
@@ -8,9 +9,58 @@ function createCells(numberOfCells) {
         let gridCell = document.createElement("div");
         gridCell.classList.add("cell")
         gridContainer.appendChild(gridCell);
+        gridCell.addEventListener("mouseenter", () => {
+            gridCell.classList.add("hoverCell");
+        });
+        gridCell.addEventListener("mouseleave", () => {
+            gridCell.classList.remove("hoverCell");
+        });
         i++;
     }
 
 }
 
 createCells(16);
+*/
+
+function setRows(numberOfRows) {
+    gridContainer.style.cssText = `grid-template-rows: repeat(${numberOfRows}, minmax(10px, 50%))`;
+}
+
+function setColumns(numberOfColumns) {
+    gridContainer.style.cssText = `grid-template-columns: repeat(${numberOfColumns}, minmax(10px, 50%))`;
+}
+
+function createCell() {
+    let gridCell = document.createElement("div");
+    gridCell.classList.add("cell");
+    gridContainer.appendChild(gridCell);
+    gridCell.addEventListener("mouseenter", () => {
+        gridCell.classList.add("hoverCell");
+    });
+    gridCell.addEventListener("mouseleave", () => {
+        gridCell.classList.remove("hoverCell");
+    });
+}
+
+function createGrid() {
+
+    let numberOfCells = prompt("How many CELLS do you want per side?");
+    setRows(numberOfCells);
+    setColumns(numberOfCells);
+
+    let i = 0;
+
+    while (i < (numberOfCells * numberOfCells)) {
+        createCell();
+        i++;
+    }
+}
+
+createGrid();
+
+
+
+
+
+
